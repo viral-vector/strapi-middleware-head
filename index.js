5b35f53b0e0359acf6d0fe706723257d8beef37c
viral-vector <vectorviral@gmail.com>
2020-01-15 20:07:34 -0500

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+const _ = require('lodash');
+const Router = require('koa-router');
+const loadHead = require('./utils/load-head');
+const createEndpointComposer = require('./utils/composeEndpoint');
+
+/**
+ * Router hook
+ */
+module.exports = strapi => {
+
+  const composeEndpoint = createEndpointComposer(strapi);
+
+  return {
+    /**
+     * Initialize the hook
+     */
+    initialize(){
+      this.boot().then(result => {
+        const router = new Router({
+          prefix: null,
+        });
+
+        _.forEach(strapi.head.config.routes, value => {
+          composeEndpoint(value, {plugin: 'head', router });
+        });
+
+        strapi.app.use(router.routes()).use(router.allowedMethods());
+      })
+    },
+
+    /**
+     * Boot Head
+     */
+    async boot() {
+      strapi.head = await loadHead(strapi);
+
+      Object.keys(strapi.head.controllers || []).forEach(key => {
+        if (!strapi.head.controllers[key].identity) {
+          strapi.head.controllers[key].identity = key;
+        }
+      });
+
+      return strapi.head;
+    }
+
+  };
+};
---
+++ utils/load-head.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { join } = require('path');
+const { existsSync } = require('fs-extra');
+const _ = require('lodash');
+const loadFiles = require('strapi/lib/load/load-files');
+const loadConfig = require('strapi/lib/load/load-config-files');
+
+
+const glob = require('strapi/lib/load/glob');
+
+module.exports = async ({ dir }) => {
+  const headDir = join(dir, 'head');
+
+  if (!existsSync(headDir)) {
+    throw new Error(
+      `Missing app folder. Please create one in your app root directory`
+    );
+  }
+
+  const files = await loadFiles(headDir, '!(config)/**/*.*(js|json)');
+  const configs = await loadConfig(headDir, 'config/**/*.*(js|json)');
+
+  return _.merge(files, configs);
+};
